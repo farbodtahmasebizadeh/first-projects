a1e655e2f2294b68bfd3dc5d297f0f1ac01cc5ad
Farbod Tahmasebi Zadeh Zavardehi <farbod.tahmasebi8@outlook.com>
2025-04-15 12:01:06 +0330

    Add files via upload

---
+++ first.py
@@ -0,0 +1,30 @@
+a = 120
+b = 140
+c = a+b
+print(c)
+
+a = 120
+b = 100
+c = a-b
+print(c)
+
+a = 120
+b = 140
+c = a*b
+print(c)
+
+a = 200
+b = 20
+c = a/b
+print(c)
+
+a = 100
+b = 2
+c = a**b
+print(c)
+
+a = 140
+b = 100
+c = a//b
+print(c)
+
